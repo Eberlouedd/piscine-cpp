eedb33ca7fcb3d725f6e616b3bd345cad0bc5263
Kenzy YACINI <kyacini@made-f0Cr13s2.clusters.42paris.fr>
2023-09-23 23:55:02 +0200

    comp

--- cpp0/ex02/Account.cpp
+++ cpp0/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: kyacini <kyacini@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 20:57:25 by kyacini           #+#    #+#             */
-/*   Updated: 2023/09/23 21:57:03 by kyacini          ###   ########.fr       */
+/*   Updated: 2023/09/23 22:37:06 by kyacini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
-Account::Account(int initial_deposit) : _amount(initial_deposit)
+Account::Account(int initial_deposit) : _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0), _accountIndex(Account::_nbAccounts)
 {
+	std::cout << "index:" << _nbAccounts << ";amounts:" << _amount << "created;\n";
+	Account::_nbAccounts++;
 }
 
 int Account::getNbAccounts()
@@ -46,16 +48,26 @@ bool	Account::makeWithdrawal(int withdrawal)
 {
 	if(_amount - withdrawal >= 0)
 	{
+		std::cout << "index:" << _accountIndex << ";p_amounts:" << _amount << ";withdrawal:" << withdrawal;
 		_amount -= withdrawal;
+		Account::_totalNbWithdrawals++;
+		_nbWithdrawals++;
+		std::cout << ";amount:" << _amount << ";nb_withdrawals\n" << _nbWithdrawals;
 		return (true);
 	}
 	else
+	{
+		std::cout << "index:" << _accountIndex << ";p_amounts:" << _amount << ";withdrawal: refused\n";
 		return (false);
+	}
 }
 
 void	Account::makeDeposit( int deposit )
 {
+
 	_amount += deposit;
+	Account::_totalNbDeposits++;
+	_nbDeposits++;
 }
 
 int		Account::checkAmount() const
